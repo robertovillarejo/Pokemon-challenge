46629e26b4fa653f88bc1a14f15f68f54223b14c
Jorge Patricio Ortega Lopez <expectise.de.mexico@gmail.com>
2024-12-12 18:07:57 +0000

    Apply module: spring-boot-cache

---
+++ src/main/java/mx/com/bankaya/wire/cache/infrastructure/secondary/CacheConfiguration.java
@@ -0,0 +1,8 @@
+package mx.com.bankaya.wire.cache.infrastructure.secondary;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+class CacheConfiguration {}
