2909f6cef670da921c2b2eb53a1241e4de2abac8
Jorge Patricio Ortega Lopez <expectise.de.mexico@gmail.com>
2024-12-12 18:07:58 +0000

    Apply module: ehcache-java-config

---
+++ src/test/java/mx/com/bankaya/wire/cache/infrastructure/secondary/CacheConfigurationTest.java
@@ -0,0 +1,53 @@
+package mx.com.bankaya.wire.cache.infrastructure.secondary;
+
+import static org.mockito.Mockito.*;
+
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.configuration.Configuration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import mx.com.bankaya.UnitTest;
+
+@UnitTest
+@ExtendWith(MockitoExtension.class)
+class CacheConfigurationTest {
+
+  private static final CacheConfiguration cacheConfiguration = new CacheConfiguration(new EhcacheProperties());
+
+  @Mock
+  private CacheManager cm;
+
+  @Mock
+  private Cache<Object, Object> cache;
+
+  @Mock
+  private Configuration<Object, Object> customConfig;
+
+  @Test
+  void shouldCreateDefaultCache() {
+    cacheConfiguration.createCache(cm, "shouldCreateDefaultCache");
+    verify(cm).createCache(eq("shouldCreateDefaultCache"), any());
+  }
+
+  @Test
+  void shouldCreateCustomCache() {
+    CacheConfiguration.createCache(cm, "shouldCreateCustomCache", customConfig);
+    verify(cm).createCache("shouldCreateCustomCache", customConfig);
+  }
+
+  @Test
+  void shouldClearExistingCache() {
+    // given an existing cache "test"
+    doReturn(cache).when(cm).getCache("shouldClearExistingCache");
+
+    // when attempting to create a new cache "test"
+    CacheConfiguration.createCache(cm, "shouldClearExistingCache", customConfig);
+
+    // then destroy and create cache "test"
+    verify(cache).clear();
+    verify(cm, never()).createCache("shouldClearExistingCache", customConfig);
+  }
+}
